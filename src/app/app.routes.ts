d3642373aec33796b8a86c65b0e66c6c08ba98cd
Udi <udigeri@gmail.com>
2023-12-03 09:32:07 +0100

    page not found created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { AboutComponent } from './views/about/about.component';
 import { TunesComponent } from './views/tunes/tunes.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'tunes', component: TunesComponent}
+    {path: 'tunes', component: TunesComponent},
+    {path: '**', component: PageNotFoundComponent}
 ];
