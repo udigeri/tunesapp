c4050ebbd33602c5164d927b9755d5af5d3338c0
Udi <udigeri@gmail.com>
2023-12-12 21:45:24 +0100

    debounce with lodash

--- src/app/components/tunes/tunes-search/tunes-search.component.ts
+++ src/app/components/tunes/tunes-search/tunes-search.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SongService } from '../../../services/song.service';
+import { debounce } from 'lodash-es';
 
 @Component({
   selector: 'tunes-search',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './tunes-search.component.html',
-  styleUrl: './tunes-search.component.scss'
+  styleUrl: './tunes-search.component.scss',
 })
-export class TunesSearchComponent implements OnInit{
-
-  @Output() newSongHaveArrived = new EventEmitter<string>()
+export class TunesSearchComponent implements OnInit {
+  @Output() newSongHaveArrived = new EventEmitter<string>();
 
   constructor(private songService: SongService) {}
 
@@ -22,4 +22,7 @@ export class TunesSearchComponent implements OnInit{
     this.songService.addSong(query);
   }
 
+  search = debounce((query: string): void => {
+    console.log(query)
+  }, 500)
 }
